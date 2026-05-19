15e5fc1faf1d07eaeb9909803ea9ebfa2fb0b6af
Juliano Timóteo <julianotimoteo@usinapitangueiras.com.br>
2026-05-19 11:43:14 -0300

    Fix: Melhorar CORS no backend

--- server.js
+++ server.js
@@ -115,7 +115,8 @@ const server = http.createServer(async (req, res) => {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(204);
